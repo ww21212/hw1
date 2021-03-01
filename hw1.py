fd1869fefa8404c242c7a2cd78b31e672bac01ed
your name <your email>
2021-03-01 02:17:23 -0800

    add hw1.py

--- hw1.py
+++ hw1.py
@@ -28,18 +28,17 @@ with open(cwb_filename) as csvfile:
 # target_data = data[:10]
 targets = ["C0A880", "C0F9A0", "C0G640", "C0R190", "C0X260"]
 result = []
-for target in targets:
-   target_data = list(filter(lambda item: item['station_id'] == target, data))
-   pres_column = [column['PRES'] for column in target_data]
+for target in targets:                                                           # target from "C0A880" to "C0F9A0"
+   target_data = list(filter(lambda item: item['station_id'] == target, data))   # select imformations in the target row
+   pres_column = [column['PRES'] for column in target_data]                      # select the values in column "PRES"
 
    num = 0
    pres_total = 0
-   for pres_value in pres_column:
+   for pres_value in pres_column:                                                # pres_value from the 1st value to the last and calculate the average
       if float(pres_value) != -99 and float(pres_value) != -999:
          num += 1
          pres_total += float(pres_value)
          pres_mean = pres_total / num
-      #print(num)
    if num != 0:
       result.append([target,round(pres_mean, 1)])
    else:
