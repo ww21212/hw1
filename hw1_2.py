52060f104723030e7f8e5f2c193e4c237e66ab74
your name <your email>
2021-03-03 10:02:56 -0800

    add hw1_2.py

--- hw1_2.py
+++ hw1_2.py
@@ -29,12 +29,12 @@ with open(cwb_filename) as csvfile:
 targets = ["C0A880", "C0F9A0", "C0G640", "C0R190", "C0X260"]
 result = []
 i = 0
-for target in targets:  # target from "C0A880" to "C0F9A0"
+for target in targets:                                                                              # target from "C0A880" to "C0F9A0"
     target_data = list(filter(lambda item: item['station_id'] == target, data))
     num = 0
     pres_total = 0
-    for i in range(len(target_data)):   # i from 0 to [number of data of target - 1]
-        if float(target_data[i].get("PRES")) != -99 and float(target_data[i].get("PRES")) != -999: # discard the value = -99 or -999
+    for i in range(len(target_data)):                                                               # i from 0 to [number of data of target - 1]
+        if float(target_data[i].get("PRES")) != -99 and float(target_data[i].get("PRES")) != -999:  # discard the value = -99 or -999
             pres_total += float(target_data[i].get("PRES"))
             num += 1
     if num != 0:
